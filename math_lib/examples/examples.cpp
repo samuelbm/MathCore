a7d32fa7ae037bb9570c35e2efbe4c298f5ca8e8
Samuel Brin-Marquis <samuelbrinmarquis@live.ca>
2025-09-29 21:51:14 -0400

    done with math_utils

--- math_lib/examples/examples.cpp
+++ math_lib/examples/examples.cpp
@@ -3,13 +3,20 @@
 #include "linear_algebra.hpp"
 #include "matrix_utils.hpp"
 
+
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <cmath>
 int main() 
 {
     Matrix A(2, 3);
-    A(0, 0) = 1.0; A(0, 1) = 1.0; A(0, 2) = 1.0;
-    A(1, 0) = 2.0; A(1, 1) = 1.0; A(1, 2) = 1.0;
+    A(0, 0) = -1.0; A(0, 1) = 0.0012345; A(0, 2) = 1.0;
+    A(1, 0) = 2.01; A(1, 1) = 1.0004; A(1, 2) = 12348.12355;
 
-    std::cout << convert_matrix_to_string(A, 3) << std::endl;
+    std::cout << convert_matrix_to_string(A) << std::endl;
 
     return 0;
 }
--- math_lib/include/matrix_utils.hpp
+++ math_lib/include/matrix_utils.hpp
@@ -1,4 +1,8 @@
 #include "matrix.hpp"
 #include <iostream>
 
-std::string convert_matrix_to_string(Matrix const& matrix, size_t precision_digits);
+std::string convert_matrix_to_string(Matrix const& matrix, size_t precision=3);
+static void compute_maximum_number_of_digits(Matrix const& matrix, size_t& max_magnitude, size_t& min_magnitude, size_t precision);
+static void compute_number_of_digits(double value, size_t& max_magnitude, size_t& min_magnitude, size_t precision);
+static void format_all_matrix_entries(Matrix const& matrix, size_t max_magnitude, size_t min_magnitude, size_t precision, std::ostringstream& oss);
+static void format_matrix_entry(double value, size_t max_magnitude, size_t min_magnitude, size_t precision, std::ostringstream& oss);
